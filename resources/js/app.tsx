858d865738b6f0640adc38971d706793354a03fb
Hoang Kiem <kiemhd@outlook.com>
2025-01-08 15:18:44 +0700

    feat: add shadcn components

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,14 +5,14 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Shadcn Laravel Admin';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) =>
         resolvePageComponent(
-            `./Pages/${name}.tsx`,
-            import.meta.glob('./Pages/**/*.tsx'),
+            `./pages/${name}.tsx`,
+            import.meta.glob('./pages/**/*.tsx'),
         ),
     setup({ el, App, props }) {
         const root = createRoot(el);
