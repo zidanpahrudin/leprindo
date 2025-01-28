48d61b840e3c0fd1c3dd84959f1d31acd0f0e18a
Hoang Kiem <kiemhd@outlook.com>
2025-01-28 22:07:58 +0700

    chore: clean

--- resources/js/pages/auth/sign-in/components/user-auth-form.tsx
+++ resources/js/pages/auth/sign-in/components/user-auth-form.tsx
@@ -3,7 +3,6 @@ import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Link } from '@inertiajs/react'
-import { IconBrandFacebook, IconBrandGithub } from '@tabler/icons-react'
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 import {
